cab927d1ad64059f1b15243754b512e31ae15374
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 16:13:03 -0500

    Added Dataset class

---
+++ src/Java/neuralNetwork/Dataset.java
@@ -0,0 +1,31 @@
+package Java.neuralNetwork;
+
+import java.util.*;
+
+public class Dataset {
+	Map<float[], float[]> data = new HashMap<float[], float[]>();
+	int size;
+	public Dataset(float[][] in, float[][] target)
+	{
+		assert(in.length == target.length) : in.length + " != " + target.length;
+		size = in.length;
+		for(int a = 0; a < size; a++)
+		{
+			data.put(in[a], target[a]);
+		}
+	}
+	public List<float[]> getData()
+	{
+		List<float[]> list = new ArrayList<>(data.keySet());
+		Collections.shuffle(list);
+		return list;
+	}
+	public float[] get(float[] in)
+	{
+		return data.get(in);
+	}
+	public int size()
+	{
+		return size;
+	}
+}
