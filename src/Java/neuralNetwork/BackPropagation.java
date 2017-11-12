502a519ffc122074a40a55000930660cbdab9cda
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 16:15:24 -0500

    Several new changes
    1. Changed constructor to take in Dataset
    2. Fixed inability to train more than 1 hidden layer.
    3. Modified for Dataset

--- src/Java/neuralNetwork/BackPropagation.java
+++ src/Java/neuralNetwork/BackPropagation.java
@@ -4,19 +4,16 @@ import java.util.*;
 
 public class BackPropagation extends Training {
 	private NeuralNetwork network;
-	private float[][] input, target;
 	private float learnRate;
 	Cost cost = Cost.quadratic();
 	
 	public BackPropagation(
 			NeuralNetwork n, 
-			float[][] in, 
-			float[][] target,
+			Dataset data,
 			float learnRate)
 	{
+		super(data);
 		this.network = n;
-		this.input = in;
-		this.target = target;
 		this.learnRate = learnRate;
 	}
 	public void train(int epochs)
@@ -32,17 +29,15 @@ public class BackPropagation extends Training {
 	public float iterate()
 	{
 		float tmp = 0;
-		int n = network.weights.length;
+		int n = network.weights.length, size = data.size();
 		float[][] nebla_b = new float[n][];
 		for(int a = 0; a < nebla_b.length; a++)
 		{
 			nebla_b[a] = new float[network.get(a+1).size()];
 		}
-		for(int i = 0; i < input.length; i++)
-		{
-			float[] x = input[i], y = target[i];
+		data.getData().forEach(x -> {
 			float[] out = network.feedForward(x);
-			float[] delta = cost.delta(out, y, network.layers[n]);
+			float[] delta = cost.delta(out, data.get(x), network.layers[n]);
 			
 			nebla_b[n-1] = Utils.add(nebla_b[n-1], delta);
 			network.weights[n-1].deltaWeights = Matrix.add(
@@ -51,28 +46,52 @@ public class BackPropagation extends Training {
 							new Matrix(delta), 
 							new Matrix(network.get(n-1).outputs
 									).transpose()
-							).transpose());
-			for(int l = 2; l < n; l++)
+							).transpose()
+					);
+			for(int l = 2; l <= n; l++)
 			{
 				delta = Matrix.haramardProduct(
 						Matrix.multiply(
-								network.weights[n-l].weights.transpose(), 
+								network.weights[n-l+1].weights, 
 								new Matrix(delta)
 								), 
-						new Matrix(network.get(n-l).outputs)).matrix();
+						new Matrix(network.get(n-l+1).outputs)).matrix();
 				nebla_b[n-l] = Utils.add(nebla_b[n-l], delta);
 				network.weights[n-l].deltaWeights = Matrix.add(
 						network.weights[n-l].deltaWeights, 
 						Matrix.multiply(
 								new Matrix(delta), 
-								new Matrix(network.get(n-l-1).outputs).transpose())
+								new Matrix(network.get(n-l).outputs
+										).transpose()
+								).transpose()
 						);
 			}
-		}
+/*			for(int l = 2; l <= n; l++)
+			{
+				System.out.println(n-l);
+				new Matrix(delta).print();
+				network.weights[n-l+1].weights.transpose().print();
+				delta = Matrix.haramardProduct(
+						Matrix.multiply(
+								network.weights[n-l+1].weights,
+								new Matrix(delta)
+								), 
+						new Matrix(network.get(n-l+1).outputs)
+						).matrix();
+				nebla_b[n-l] = Utils.add(nebla_b[n-l], delta);
+				network.weights[n-l].deltaWeights = Matrix.add(
+						network.weights[n-l].deltaWeights, 
+						Matrix.multiply(
+								new Matrix(delta),
+								new Matrix(network.get(n-l).outputs).transpose()
+								).transpose()
+						);
+			}*/
+		});
 		for(int a = 0; a < n; a++)
 		{
-			network.weights[a].adjustWeights(learnRate, input.length);
-			network.get(a+1).updateBiases(nebla_b[a], learnRate, input.length);
+			network.weights[a].adjustWeights(learnRate, size);
+			network.get(a+1).updateBiases(nebla_b[a], learnRate, size);
 		}
 		return tmp;
 	}
