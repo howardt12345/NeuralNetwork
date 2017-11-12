b4e9e3d269c9ec8a951fcd46c687389a1f7d9c8e
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 16:23:51 -0500

    Change to delta function

--- src/Java/neuralNetwork/Cost.java
+++ src/Java/neuralNetwork/Cost.java
@@ -2,7 +2,7 @@ package Java.neuralNetwork;
 
 public abstract class Cost {
 	public abstract float f(float[] A, float[] E);
-	public abstract float[] delta(float[] A, float[] E, Layer l);
+	public abstract float[] delta(float[] A, float[] E);
 	private Cost () {}
 	
 	public static Cost quadratic()
@@ -16,8 +16,8 @@ public abstract class Cost {
 					tmp += 0.5f*Math.pow(A[a] - E[a], 2);
 				return tmp;
 			}
-			public float[] delta(float[] A, float[] E, Layer l) {
-				return Utils.multiply(Utils.subtract(A, E), l.derivatives());
+			public float[] delta(float[] A, float[] E) {
+				return Utils.subtract(A, E);
 			}
 		};
 	}
