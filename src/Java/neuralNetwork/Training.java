892236f7802174b1064a55964553c603d786d73b
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 16:13:25 -0500

    Training superclass now contains Dataset object

--- src/Java/neuralNetwork/Training.java
+++ src/Java/neuralNetwork/Training.java
@@ -2,6 +2,12 @@ package Java.neuralNetwork;
 
 public class Training {
 	private float error = 1;
+	protected Dataset data;
+	
+	protected Training(Dataset data)
+	{
+		this.data = data;
+	}
 	
 	public void train()
 	{
