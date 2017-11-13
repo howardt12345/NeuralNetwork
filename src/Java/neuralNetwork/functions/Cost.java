5483e6d423e178766c1e6e72fea8c05681f03c64
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 19:08:25 -0500

    Added cross entropy cost function

--- src/Java/neuralNetwork/functions/Cost.java
+++ src/Java/neuralNetwork/functions/Cost.java
@@ -10,15 +10,42 @@ public abstract class Cost {
 	public static Cost mst()
 	{
 		return new Cost() {
-			
-			public float f(float[] A, float[] E) {
-				assert(A.length == E.length);
+			@Override
+			public float f(float[] A, float[] E) 
+			{
+				assert(A.length == E.length) : A.length + " != " + E.length;
 				float tmp = 0;
 				for(int a = 0; a < A.length; a++)
 					tmp += Math.pow(A[a] - E[a], 2);
 				return 0.5f*tmp;
 			}
-			public float[] delta(float[] A, float[] E) {
+			@Override
+			public float[] delta(float[] A, float[] E) 
+			{
+				return Utils.subtract(A, E);
+			}
+		};
+	}
+	public static Cost crossEntropy()
+	{
+		return new Cost() {
+			@Override
+			public float f(float[] A, float[] E) 
+			{
+				assert(A.length == E.length) : A.length + " != " + E.length;
+				float tmp = 0;
+				for(int a = 0; a < A.length; a++)
+					tmp += -(E[a]  *Utils.ln(A[a]) + (1-E[a])*Utils.ln(1 - A[a]));
+				return tmp;
+			}
+			@Override
+			public float[] delta(float[] A, float[] E) 
+			{
+/*				assert(A.length == E.length) : A.length + " != " + E.length;
+				float[] tmp = new float[A.length];
+				for(int a = 0; a < tmp.length; a++)
+					tmp[a] = (A[a] - E[a])/((1 - A[a])*A[a]);
+				return tmp;*/
 				return Utils.subtract(A, E);
 			}
 		};
