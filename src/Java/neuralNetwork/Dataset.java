22b66e548b01b45b7ccb188c6d64ffb6858584f2
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-12 20:40:15 -0500

    Database for String

--- src/Java/neuralNetwork/Dataset.java
+++ src/Java/neuralNetwork/Dataset.java
@@ -14,6 +14,21 @@ public class Dataset {
 			data.put(in[a], target[a]);
 		}
 	}
+	public Dataset(String[] in, String[] target)
+	{
+		assert(in.length == target.length) : in.length + " != " + target.length;
+		size = in.length;
+		for(int i = 0; i < size; i++)
+		{
+			String[] rawIn = in[i].split(" "), rawTarget = target[i].split(" ");
+			float[] parsedIn = new float[rawIn.length], parsedTarget = new float[rawTarget.length];
+			for(int a = 0; a < parsedIn.length; a++)
+				parsedIn[a] = Float.parseFloat(rawIn[a]);
+			for(int a = 0; a < parsedTarget.length; a++)
+				parsedTarget[a] = Float.parseFloat(rawTarget[a]);
+			data.put(parsedIn, parsedTarget);
+		}
+	}
 	public List<float[]> getData()
 	{
 		List<float[]> list = new ArrayList<>(data.keySet());
