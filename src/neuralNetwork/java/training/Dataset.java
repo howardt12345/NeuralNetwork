8075ebb6aa15b2eafcc92a75ddc669812065ab64
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-18 17:05:56 -0500

    Reading/Writing Dataset to/from file added

--- src/neuralNetwork/java/training/Dataset.java
+++ src/neuralNetwork/java/training/Dataset.java
@@ -1,6 +1,7 @@
 package neuralNetwork.java.training;
 
 import java.util.*;
+import java.io.*;
 
 public class Dataset {
 	private Map<float[], float[]> data = new HashMap<float[], float[]>();
@@ -29,6 +30,21 @@ public class Dataset {
 			data.put(parsedIn, parsedTarget);
 		}
 	}
+	public Dataset(File file) throws IOException
+	{
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		String line1, line2;
+		while((line1 = in.readLine()) != null && (line2 = in.readLine()) != null)
+		{
+			String[] rawIn = line1.split(" "), rawTarget = line2.split(" ");
+			float[] parsedIn = new float[rawIn.length], parsedTarget = new float[rawTarget.length];
+			for(int a = 0; a < parsedIn.length; a++)
+				parsedIn[a] = Float.parseFloat(rawIn[a]);
+			for(int a = 0; a < parsedTarget.length; a++)
+				parsedTarget[a] = Float.parseFloat(rawTarget[a]);
+			data.put(parsedIn, parsedTarget);
+		}
+	}
 	public List<float[]> getData()
 	{
 		List<float[]> list = new ArrayList<>(data.keySet());
@@ -42,5 +58,25 @@ public class Dataset {
 	public int size()
 	{
 		return size;
+	}
+	public void toFile(String filename) throws IOException
+	{
+		FileWriter fw = new FileWriter(new File(filename));
+		String s = "";
+		data.forEach((x, y) -> {
+			try {
+				for(int a = 0; a < x.length; a++)
+					fw.write(x[a] + " ");
+				fw.write("\r\n");
+				for(int a = 0; a < y.length; a++)
+					fw.write(y[a] + " ");
+				fw.write("\r\n");
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		});
+		fw.close();
 	}
 }
