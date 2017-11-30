dfdc67b1730faf73a5b4261fc2122b5eea0d15dc
howardt12345 <howardt12345@users.noreply.github.com>
2017-11-29 20:26:42 -0500

    Added eigen

--- src/main/cpp/neuralNetwork.h
+++ src/main/cpp/neuralNetwork.h
@@ -2,8 +2,6 @@
 #include "stdafx.h"
 #include <iostream>
 
-#define size(x) (sizeof(x)/sizeof((x)[0]))
-
 namespace neuralNetwork
 {
 	namespace functions 
@@ -26,27 +24,5 @@ namespace neuralNetwork
 	}
 	class Utils 
 	{
-	};
-	class Matrix 
-	{
-	protected:
-		float* matrix;
-		int rows, columns;
-	public:
-		Matrix(float matrix[]);
-		Matrix(float *matrix, int rows, int columns);
-		Matrix(int rows, int columns);
-		Matrix(int length);
-		static Matrix identity(int rows, int columns);
-		static Matrix diagonal(float values[]);
-		float get(int row, int column) { return matrix[row*columns + column]; }
-		float get(int index) { return matrix[index]; }
-		void set(float value, int row, int column) { matrix[row*columns + column] = value; }
-		int getRows() { return rows; }
-		int getColumns() { return columns; }
-		int length() { return rows*columns; }
-		float* getMatrix() { return matrix; }
-		void print();
-		~Matrix() { delete[] matrix; }
 	};
 }
