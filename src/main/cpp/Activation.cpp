bd6eb953311d7a998ff9f6ddd8717eccf9401975
howardt12345 <howardt12345@users.noreply.github.com>
2017-12-02 19:30:46 -0500

    Implemented activation functions

--- src/main/cpp/Activation.cpp
+++ src/main/cpp/Activation.cpp
@@ -1,66 +1,173 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "neuralNetwork.h"
 #include "Activation.h"
 
 using namespace neuralNetwork::functions::activation;
+using namespace std;
 
-/*
-MatrixXf [name]::f(MatrixXf x)
+MatrixXf Activation::f(MatrixXf x)
 {
-return MatrixXf(1, 1);
+	return x;
 }
-MatrixXf [name]::der(MatrixXf x)
+MatrixXf Activation::der(MatrixXf x)
 {
-return MatrixXf(1, 1);
+	return x;
 }
-*/
-
 
 MatrixXf Identity::f(MatrixXf x)
 {
-	return MatrixXf();
+	return x;
 }
 MatrixXf Identity::der(MatrixXf x)
 {
-	return MatrixXf();
+	return MatrixXf::Ones(x.rows(), x.cols());
 }
-MatrixXf BinaryStep::f(MatrixXf x)
+
+float Binary::activation(float x)
 {
-	return MatrixXf(1, 1);
+	return x > 0 ? 1.0f : 0.0f;
 }
-MatrixXf BinaryStep::der(MatrixXf x)
+float Binary::derivative(float x)
 {
-	return MatrixXf(1, 1);
+	return x != 0 ? 0.0f : NAN;
+}
+MatrixXf Binary::f(MatrixXf x)
+{
+	return x.unaryExpr(ptr_fun(&Binary::activation));
+}
+MatrixXf Binary::der(MatrixXf x)
+{
+	return x.unaryExpr(ptr_fun(&Binary::derivative));
+}
+
+float Sigmoid::activation(float x)
+{
+	return 1 / (1 + exp(-x));
+}
+float Sigmoid::derivative(float x)
+{
+	return activation(x)*(1 - activation(x));
 }
 MatrixXf Sigmoid::f(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.unaryExpr(ptr_fun(&Sigmoid::activation));
 }
 MatrixXf Sigmoid::der(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.unaryExpr(ptr_fun(&Sigmoid::derivative));
+}
+
+float ReLu::activation(float x)
+{
+	return x > 0 ? x : 0.0f;
 }
 MatrixXf ReLu::f(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.unaryExpr(ptr_fun(&ReLu::activation));
 }
 MatrixXf ReLu::der(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return Binary().f(x);
+}
+
+float LeakyReLu::activation(float x)
+{
+	return x > 0 ? x : 0.01f*x;
+}
+float LeakyReLu::derivative(float x)
+{
+	return x > 0 ? 1 : 0.01f;
 }
 MatrixXf LeakyReLu::f(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.unaryExpr(ptr_fun(&LeakyReLu::activation));
 }
 MatrixXf LeakyReLu::der(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.unaryExpr(ptr_fun(&LeakyReLu::derivative));
 }
+
+struct PReLuActivation
+{
+	float operator()(float x, float a) const
+	{
+		return x > 0 ? x : a*x;
+	}
+};
+struct PReLuDerivative
+{
+	float operator()(float x, float a) const
+	{
+		return x > 0 ? 1 : a;
+	}
+};
 MatrixXf PReLu::f(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.binaryExpr(MatrixXf::Constant(x.rows(), x.cols(), α), 
+		PReLuActivation());
 }
 MatrixXf PReLu::der(MatrixXf x)
 {
-	return MatrixXf(1, 1);
+	return x.binaryExpr(MatrixXf::Constant(x.rows(), x.cols(), α), 
+		PReLuDerivative());
+}
+
+float TanH::derivative(float x)
+{
+	return 1 - pow(tanhf(x), 2);
+}
+MatrixXf TanH::f(MatrixXf x)
+{
+	return x.array().tanh();
+}
+MatrixXf TanH::der(MatrixXf x)
+{
+	return x.unaryExpr(ptr_fun(&TanH::derivative));
+}
+
+struct SoftMaxActivation
+{
+	float operator()(float x, float a) const
+	{
+		return x / a;
+	}
+};
+
+MatrixXf SoftMax::f(MatrixXf x)
+{
+	MatrixXf exp = x.array().exp();
+	MatrixXf sums = MatrixXf::Constant(x.rows(), x.cols(), exp.sum());
+	return exp.binaryExpr(sums, SoftMaxActivation());
+}
+MatrixXf SoftMax::der(MatrixXf x)
+{
+	MatrixXf f = SoftMax().f(x),
+		δ = MatrixXf::Identity(x.rows()*x.cols(), x.rows()*x.cols()),
+		A = MatrixXf(x.rows(), x.cols());
+	for (int a = 0; a < x.rows(); a++)
+		for (int b = 0; b < x.cols(); b++)
+			A(a) = f(a)*δ(a, b) - f(b);
+	return A;
+}
+
+MatrixXf SoftPlus::f(MatrixXf x)
+{
+	return (x.array().exp() + 1).array().log();
+}
+MatrixXf SoftPlus::der(MatrixXf x)
+{
+	return Sigmoid().f(x);
+}
+
+float Swish::derivative(float x)
+{
+	return x > 0 ? x : 0.01f;
+}
+MatrixXf Swish::f(MatrixXf x)
+{
+	return x.array() * Sigmoid().f((x.array() * β)).array();
+}
+MatrixXf Swish::der(MatrixXf x)
+{
+	return (β * f(x).array()) + Sigmoid().f((β * x.array())).array()*(1 - β * f(x).array());
 }
--- src/main/cpp/Activation.h
+++ src/main/cpp/Activation.h
@@ -13,49 +13,90 @@ namespace neuralNetwork
 			class Activation
 			{
 			public:
-				virtual MatrixXf f(MatrixXf x) = 0;
-				virtual MatrixXf der(MatrixXf x) = 0;
+				virtual MatrixXf f(MatrixXf x);
+				virtual MatrixXf der(MatrixXf x);
 			};
-			static class Identity : public Activation
+			class Identity : public Activation
 			{
 			public:
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} identity;
-			static class BinaryStep : public Activation
+			};
+			class Binary : public Activation
 			{
+			private:
+				static float activation(float x);
+				static float derivative(float x);
 			public:
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} binaryStep;
-			static class Sigmoid : public Activation
+			};
+			class Sigmoid : public Activation
 			{
+			private:
+				static float activation(float x);
+				static float derivative(float x);
 			public:
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} sigmoid;
-			static class ReLu : public Activation
+			};
+			class ReLu : public Activation
 			{
+			private:
+				static float activation(float x);
 			public:
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} reLu;
-			static class LeakyReLu : public Activation
+			};
+			class LeakyReLu : public Activation
 			{
+			private:
+				static float activation(float x);
+				static float derivative(float x);
 			public:
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} leakyReLu;
-			static class PReLu : public Activation
+			};
+			class PReLu : public Activation
 			{
 			private:
 				float α;
 			public:
-				PReLu() { α = 0.01f; }
-				PReLu(float α) { this->α = α ; }
+				PReLu(float α) { this->α = α; }
 				MatrixXf f(MatrixXf x);
 				MatrixXf der(MatrixXf x);
-			} pReLu;
+			};
+			class TanH : public Activation
+			{
+			private:
+				static float derivative(float x);
+			public:
+				MatrixXf f(MatrixXf x);
+				MatrixXf der(MatrixXf x);
+			};
+			class SoftMax : public Activation
+			{
+			public:
+				MatrixXf f(MatrixXf x);
+				MatrixXf der(MatrixXf x);
+			};
+			class SoftPlus : public Activation
+			{
+			public:
+				MatrixXf f(MatrixXf x);
+				MatrixXf der(MatrixXf x);
+			};
+			
+			class Swish : public Activation
+			{
+			private:
+				float derivative(float x);
+				float β;
+			public:
+				Swish(float β) { this->β = β; }
+				MatrixXf f(MatrixXf x);
+				MatrixXf der(MatrixXf x);
+			};
 		}
 	}
 }
